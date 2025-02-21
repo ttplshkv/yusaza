dc439b93acbe3ee366a827ae59b2106eda116048
root <tpleshakova@icloud.com>
2025-02-21 17:56:48 +0300

    merged and fixed: modal window

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ProductList from './components/ProductList';
 import CartPage from './pages/CartPage';
 import {CartProvider} from "./context/CartContext";
 import CartButton from "./components/buttons/CartButton/CartButton";
-import MainButton from "./components/buttons/MainButton";
 import OrdersPage from "./pages/OrdersPage";
 
 function App() {
@@ -13,32 +12,37 @@ function App() {
 
     return (
         <div className="App">
-            {location.pathname === '/' && ( // Показываем CartButton и кнопки только на главной странице
-                <div>
-                    <CartButton/>
-                    <Link to="/">
-                        <div style={{float: "left"}}>
-                            <button className="btn" onClick={() => setCategory("pizza")}>Пицца</button>
-                            <button className="btn" onClick={() => setCategory("sushi")}>Суши</button>
-                        </div>
-                    </Link>
-                </div>
-            )}
+            <CartProvider>
+                {location.pathname === '/' && (
+                    <div>
+                        <CartButton/>
+                        <Link to="/">
+                            <div style={{float: "left"}}>
+                                <button className="btn" onClick={() => setCategory("pizza")}>Пицца</button>
+                                <button className="btn" onClick={() => setCategory("sushi")}>Суши</button>
+                            </div>
+                        </Link>
+                    </div>
+                )}
+
+                {/* Оборачиваем нужные маршруты в один Routes */}
+                <Routes>
+                    <Route path="/" element={
+                        <ProductList category={category}/>
+                    }/>
+                    <Route path="/cart" element={
+                        <CartPage/>
+                    }/>
+                </Routes>
+            </CartProvider>
 
             <Routes>
-                <Route path="/" element={
-                    <CartProvider>
-                        <ProductList category={category}/>
-                    </CartProvider>
-                }/>
-                <Route path="/cart" element={
-                    <CartProvider>
-                        <CartPage/>
-                    </CartProvider>
+                <Route path="/orders" element={
+                    <OrdersPage isCompleted={false}/>
                 }/>
 
-                <Route path="/orders" element={
-                    <OrdersPage/>
+                <Route path="/orders/complete" element={
+                    <OrdersPage isCompleted={true}/>
                 }/>
             </Routes>
         </div>
