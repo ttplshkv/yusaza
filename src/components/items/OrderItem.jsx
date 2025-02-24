657758608ea11352f0408a0725e03e9f75cb7a1e
Yusuf-corinnem <yusuf.shavkatov@icloud.com>
2025-02-24 17:02:17 +0300

    improve ordersPage

--- src/components/items/OrderItem.jsx
+++ src/components/items/OrderItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { format } from "date-fns";
+import {format} from "date-fns";
 import useOrderProducts from "../../hooks/useOrderProducts";
 import useOrderUpdate from "../../hooks/useOrderUpdate";
+import "../../styles/styles.css";
 
-const OrderItem = ({ order }) => {
-    const { products, error } = useOrderProducts(order);
+const OrderItem = ({order}) => {
+    const {products, error} = useOrderProducts(order);
     const {update, errorUpdate} = useOrderUpdate();
 
-    if (error) return <div className="error">{error}</div>;
+    if (error || errorUpdate) return <div className="error">{error ? error : errorUpdate}</div>;
 
     // Форматирование дат
     const createdDate = order?.createdAt ? format(new Date(order.createdAt), "dd.MM.yyyy") : "нет данных";
@@ -17,40 +18,42 @@ const OrderItem = ({ order }) => {
     const completedTime = order?.completedAt ? format(new Date(order.completedAt), "HH:mm") : "нет данных";
 
     return (
-        <div className="border p-4 rounded-lg shadow-md">
-            <h2 className="text-lg font-bold">Заказ №{order._id}</h2>
-            <p><strong>Имя заказчика:</strong> {order.personName}</p>
-            <p><strong>Номер телефона:</strong> {order.number}</p>
-            <p><strong>Адрес доставки:</strong> {order.address}</p>
-            <p><strong>Сумма заказа:</strong> {order.fullAmount} ₽</p>
+        <div className="border border-black border-2 rounded-lg mt-5">
+            <h2 className="border-bottom border-black border-2 text-lg font-bold">Заказ №{order._id}</h2>
+            <p className="custom-border-bottom"><strong>Имя заказчика:</strong> {order.personName}</p>
+            <p className="custom-border-bottom"><strong>Номер телефона:</strong> {order.number}</p>
+            <p className="custom-border-bottom"><strong>Адрес доставки:</strong> {order.address}</p>
+            <p className="custom-border-bottom"><strong>Сумма заказа:</strong> {order.fullAmount} ₽</p>
 
-                {order.isCompleted
-                    ? <p><strong>Статус: </strong> выполнено</p>
-                    : <p><strong>Статус: </strong> в процессе...</p>
-                }
+            {order.isCompleted
+                ? <p className="custom-border-bottom"><strong>Статус: </strong> выполнено</p>
+                : <p className="custom-border-bottom"><strong>Статус: </strong> в процессе...</p>
+            }
 
-            <h3 className="mt-2 font-semibold">Товары:</h3>
-            <div className="grid gap-2">
-                {(products || []).map((product) => {
-                    const orderProduct = order.products.find(p => p.productId === product._id);
+            <div className="border-top-bottom rounded-md">
+                <h3 className="custom-border-bottom text-center mt-2 font-semibold">Товары:</h3>
+                <div className="grid gap-2">
+                    {(products || []).map((product) => {
+                        const orderProduct = order.products.find(p => p.productId === product._id);
 
-                    return (
-                        <div key={product._id} className="border p-2 rounded-md shadow-sm flex justify-between">
-                            <strong>{product.name}</strong>
-                            <span>, количество: {orderProduct?.quantity ?? "Не найдено"}</span>
-                        </div>
-                    );
-                })}
+                        return (
+                            <div key={product._id} className="custom-border-bottom border-bottom-0 flex justify-between">
+                                <strong>{product.name}: </strong>
+                                <span>{orderProduct?.quantity ?? "Не найдено"}</span>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
 
-            <h3 className="mt-2 font-semibold">Дата создания: {createdDate}</h3>
-            <h3 className="mt-2 font-semibold">Время создания: {createdTime}</h3>
+            <p className="custom-border-bottom border-top border-black border-2 border-bottom-0 mt-2 font-semibold">Дата создания: {createdDate}</p>
+            <p className="custom-border-bottom mt-2 font-semibold">Время создания: {createdTime}</p>
 
             {order.isCompleted && (
-                <div className="mt-4 p-2 bg-gray-100 rounded-lg">
-                    <h2 className="text-green-600 font-bold">Выполнено:</h2>
-                    <h3 className="font-semibold">Дата: {completedDate}</h3>
-                    <h3 className="font-semibold">Время: {completedTime}</h3>
+                <div>
+                    <h3 className="custom-border-bottom text-center">Выполнено:</h3>
+                    <p className="custom-border-bottom border-bottom-0 font-semibold">Дата: {completedDate}</p>
+                    <p className="custom-border-bottom border-bottom-0 font-semibold">Время: {completedTime}</p>
                 </div>
             )}
 
